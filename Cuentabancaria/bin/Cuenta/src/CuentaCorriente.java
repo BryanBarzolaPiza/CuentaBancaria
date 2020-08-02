b3c772a7759c9af232dd30aa130bbf826c047180
Bryan Barzola <67973954+BryanBarzolaPiza@users.noreply.github.com>
2020-08-02 18:00:20 -0500

    Add files via upload

---
+++ Cuentabancaria/bin/Cuenta/src/CuentaCorriente.java
@@ -0,0 +1,17 @@
+
+public class CuentaCorriente extends CuentaBancaria {
+	double a=0;
+	public void deposito(double dinero) {
+		
+		if (dinero <= 1000) {
+			a= (dinero *1)/100;
+			saldo = dinero+a;
+			
+		}else {
+			saldo += dinero;
+		}
+		
+		
+	}
+	
+}
